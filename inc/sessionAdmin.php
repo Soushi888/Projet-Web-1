dc9a6f604376d85538d01ff545154c08c60b3283
Soushi888 <Sacha.pignot@gmail.com>
2020-01-19 02:50:36 -0500

    update

--- inc/sessionAdmin.php
+++ inc/sessionAdmin.php
@@ -7,5 +7,7 @@ if ($_SESSION['identifiant_utilisateur'] !== "admin@magasin.com") {
     // redirection vers la page authentification.php
     // pour la saisie de l'identifiant et du mot de passe 
     header('Location: ../index.php'); }
-
+else {
+    $_SESSION['type_utilisateur'] = "Administrateur";
+}
 ?>
