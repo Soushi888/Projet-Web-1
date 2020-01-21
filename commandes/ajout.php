58e51f145a58f5e388341cd375620594f7c8b2ab
Pignot <e1995655@cmaisonneuve.qc.ca>
2020-01-21 17:28:27 -0500

    Update ajout.php

--- commandes/ajout.php
+++ commandes/ajout.php
@@ -85,20 +85,20 @@ if (isset($_POST["envoi"])) :
         <fieldset>
             <legend>Informations client</legend>
             <label for="client_id">ID :
-                <input name="client_id" id="client_id" type="number" placeholder="ID" required></label>
+                <input name="client_id" id="client_id" type="number" placeholder="ID" value="<?= isset($_SESSION["commande"]) ? $_SESSION["commande"]["info_client"]["client_id"] : "" ?>" required></label>
             <label for="client_adresse">Adresse :
-                <input name="client_adresse" id="client_adresse" type="text" required></label>
+                <input name="client_adresse" id="client_adresse" type="text" value="<?= isset($_SESSION["commande"]) ? $_SESSION["commande"]["info_client"]["client_adresse"] : "" ?>" required></label>
             <label for="client_adresse2">Adresse2 :
-                <input name="client_adresse2" id="client_adresse2" type="text"></label>
+                <input name="client_adresse2" id="client_adresse2" type="text" value="<?= isset($_SESSION["commande"]) ? $_SESSION["commande"]["info_client"]["client_adresse2"] : "" ?>"></label>
             <label for="client_ville">Ville :
-                <input name="client_ville" id="client_ville" type="text" required></label>
+                <input name="client_ville" id="client_ville" type="text" value="<?= isset($_SESSION["commande"]) ? $_SESSION["commande"]["info_client"]["client_ville"] : "" ?>" required></label>
             <label for="client_cp">Code postal :
-                <input name="client_cp" id="client_cp" type="text" required></label><br>
+                <input name="client_cp" id="client_cp" type="text" value="<?= isset($_SESSION["commande"]) ? $_SESSION["commande"]["info_client"]["client_cp"] : "" ?>" required></label><br>
         </fieldset>
         <fieldset>
             <legend>Information commande</legend>
             <label for="commande_commentaire">Commentaires : </label>
-            <textarea name="commande_commentaires" id="commande_commentaires" cols="100" rows="10"></textarea>
+            <textarea name="commande_commentaires" id="commande_commentaires" cols="100" rows="10" value="<?= isset($_SESSION["commande"]) ? $_SESSION["commande"]["info_client"]["commande_commentaires"] : "" ?>"></textarea>
         </fieldset>
         <button form="commande" type="submit" name="envoi">Commander</button>
     </form>
@@ -148,7 +148,8 @@ if (isset($_POST["envoi"])) :
                 </form>
             </section>
         </section>
-    <?php else : ?>
+    <?php elseif (isset($_SESSION["commande"]) && !isset($_SESSION["commande"]["info_commande"])) :
+        unset($_SESSION["commande"]); ?>
         <p class="erreur">Veuillez selectionner au moins un produit.</p>
     <?php endif;
     ?>
