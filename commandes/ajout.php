11f7f106a1ae5d52536574b8c8b91e3069deb1ee
Soushi888 <Sacha.pignot@gmail.com>
2020-01-21 13:15:30 -0500

    Update ajout.php

--- commandes/ajout.php
+++ commandes/ajout.php
@@ -9,18 +9,24 @@ $liste = listerProduits($conn, $recherche);
 $client_id = "";
 
 if (isset($_POST["envoi"])) :
-    $i = 1;
-    $_SESSION["commande"] = array("client_id" => $_POST["client_id"]);
+    $i = 0;
+    $_SESSION["commande"]["info_client"] = array(
+        "client_id" => $_POST["client_id"],
+        "client_adresse" => $_POST["client_adresse"],
+        "client_adresse2" => $_POST["client_adresse2"],
+        "client_ville" => $_POST["client_ville"],
+        "client_cp" => $_POST["client_cp"]
+    );
 
     foreach ($_POST as $produit => $quantite) {
-        if ($quantite != "" && $quantite != 0) {
-            $_SESSION["commande"][] = ["produit" => substr($produit, 1), "quantité" => $quantite];
+        // ++$i;
+        if ($quantite != "" && $quantite != 0 &&  is_numeric($produit)) {
+            $_SESSION["commande"][] = ["produit" => $produit, "quantité" => $quantite];
         }
-        ++$i;
     }
     // EnregistrerCommande($conn, $_SESSION["commande"], $client_id);
 ?>
-    <pre><?= var_dump($_SESSION) ?></pre>
+    <pre><?= print_r($_SESSION) ?></pre>
 <?php endif; ?>
 
 <!DOCTYPE html>
@@ -46,11 +52,6 @@ if (isset($_POST["envoi"])) :
 
     <form id="commande" action="" method="post">
         <fieldset>
-            <legend>Informations client</legend>
-            <label for="client_id">Client ID : </label>
-                <input id="client_id" name="client_id" type="number" placeholder="ID"> 
-        </fieldset>
-        <fieldset>
             <legend>Produits à commander</legend>
             <table>
                 <tr>
@@ -73,15 +74,29 @@ if (isset($_POST["envoi"])) :
                         <td><?= $row["produit_quantite"] ?></td>
                         <td><?= $row["categorie_nom"] ?></td>
                         <td>
-                            <input type="number" min="0" max="<?= $row["produit_quantite"] ?>" name="q<?= $row['produit_id'] ?>" placeholder="Quantité" form="commande">
+                            <input type="number" min="0" max="<?= $row["produit_quantite"] ?>" name="<?= $row['produit_id'] ?>" placeholder="Quantité" form="commande">
                         </td>
                     </tr>
                 <?php endforeach; ?>
             </table>
+        </fieldset>
 
-            <button form="commande" type="submit" name="envoi">Commander</button>
+        <fieldset>
+            <legend>Informations client</legend>
+            <label for="client_id">ID :
+                <input name="client_id" id="client_id" type="number" placeholder="ID" required></label>
+            <label for="client_adresse">Adresse :
+                <input name="client_adresse" id="client_adresse" type="text" required></label>
+            <label for="client_adresse2">Adresse2 :
+                <input name="client_adresse2" id="client_adresse2" type="text" required></label>
+            <label for="client_ville">Ville :
+                <input name="client_ville" id="client_ville" type="text" required></label>
+            <label for="client_cp">Code postal :
+                <input name="client_cp" id="client_cp" type="text" required></label>
+        </fieldset>
+
+        <button form="commande" type="submit" name="envoi">Commander</button>
     </form>
-    </fieldset>
 
 </body>
 
