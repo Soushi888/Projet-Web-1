8782bac2a1af4d40133c5345fb052db4b62f4e82
Pignot <e1995655@cmaisonneuve.qc.ca>
2020-01-24 11:06:52 -0500

    update

--- menu.php
+++ menu.php
@@ -1,14 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <nav>
-        
-    </nav>
-</body>
-</html>
+<nav id="main_menu">
+    <fieldset>
+        <legend>Navigation</legend>
+        <fieldset>
+            <legend>Vendeur</legend>
+            <a href="../clients/index.php">Clients</a><a href="../commandes/index.php">Commandes</a>
+        </fieldset>
+        <?php if ($_SESSION['utilisateur']["utilisateur_type"] == "gestionnaire" || $_SESSION['utilisateur']["utilisateur_type"] == "administrateur") : ?>
+            <fieldset>
+                <legend>Gestionnaire</legend>
+                <a href="../produits/index.php">Produits</a><a href="../categories/index.php">Catégories</a>
+            </fieldset>
+        <?php endif; ?>
+        <?php if ($_SESSION['utilisateur']["utilisateur_type"] == "administrateur") : ?>
+            <fieldset>
+                <legend>Administrateur</legend>
+                <a href="../utilisateurs/index.php">Utilisateurs</a>
+            </fieldset>
+        <?php endif; ?>
+        <button><a href="../deconnexion.php">Déconnexion</a></button>
+    </fieldset>
+</nav>
